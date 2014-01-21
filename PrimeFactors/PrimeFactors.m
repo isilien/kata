c9efe338fc974fc0e00fd80fcf1a46afcb552772
Isil Demir <isildemir.id@gmail.com>
2014-01-20 22:26:39 -0800

    Test case for generate(6) is now green

--- PrimeFactors/PrimeFactors.m
+++ PrimeFactors/PrimeFactors.m
@@ -12,13 +12,32 @@
 
 //
 // Given an integer "number", return a list of its prime factors in ascending order
+// Return an empty array if the given integer has no prime factors
 //
 - (NSMutableArray *)generate:(NSNumber *)number {
     
     NSMutableArray *primeFactors = [NSMutableArray array];
     
     if ([number integerValue] > 1) {
-        [primeFactors addObject:[NSNumber numberWithInt:2]];
+        
+        int quotient = [number integerValue];
+            
+        while (quotient%2 == 0) {
+            [primeFactors addObject:[NSNumber numberWithInt:2]];
+            quotient /= 2;
+            number = [NSNumber numberWithInt:quotient];
+        }
+        
+        if (quotient > 1) {            
+            [primeFactors addObject:[NSNumber numberWithInt:quotient]];
+        }
+
+        
+        
+        
+        
+        
+        
     }
     
     return primeFactors;
