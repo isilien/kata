2dfb5b015a9cf5ff34309a4c172f99467e27c356
Isil Demir <isildemir.id@gmail.com>
2014-01-20 18:01:40 -0800

    New project for prime factors kata

---
+++ PrimeFactors/PrimeFactors/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PrimeFactors
+//
+//  Created by Isil Demir on 1/20/14.
+//  Copyright (c) 2014 Isil Demir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PrimeFactors/PrimeFactors/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PrimeFactors
+//
+//  Created by Isil Demir on 1/20/14.
+//  Copyright (c) 2014 Isil Demir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
