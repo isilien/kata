3c5294b266589a8d625538b31441cfd6c7610500
Isil Demir <isildemir.id@gmail.com>
2014-01-20 22:30:15 -0800

    Created test case for generate(9)

--- PrimeFactors/PrimeFactorsTests/PrimeFactorsTests.m
+++ PrimeFactors/PrimeFactorsTests/PrimeFactorsTests.m
@@ -65,6 +65,17 @@
     
 }
 
+// Generate should return [3,3] if given 9
+- (void)testCaseForNine {
+    
+    PrimeFactors *pf = [[PrimeFactors alloc] init];
+    NSMutableArray *result = [pf generate:[NSNumber numberWithInt:9]];
+    
+    XCTAssertEqual([result objectAtIndex:0], [NSNumber numberWithInt:2], @"First element should be 3");
+    XCTAssertEqual([result objectAtIndex:1], [NSNumber numberWithInt:3], @"Second element should be 3");
+    
+}
+
 @end
 
 
