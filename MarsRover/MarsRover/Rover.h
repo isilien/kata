470f4412a99ba993a4f608a8fbb84ae4f3fd0124
Isil Demir <isildemir.id@gmail.com>
2014-03-12 15:04:27 -0700

    Created Rover class

---
+++ MarsRover/MarsRover/Rover.h
@@ -0,0 +1,18 @@
+//
+//  Rover.h
+//  MarsRover
+//
+//  Created by Isil Demir on 3/12/14.
+//  Copyright (c) 2014 Isil Demir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Rover : NSObject {
+    
+    NSString *direction;
+    
+    
+}
+
+@end
---
+++ MarsRover/MarsRover/Rover.m
@@ -0,0 +1,13 @@
+//
+//  Rover.m
+//  MarsRover
+//
+//  Created by Isil Demir on 3/12/14.
+//  Copyright (c) 2014 Isil Demir. All rights reserved.
+//
+
+#import "Rover.h"
+
+@implementation Rover
+
+@end
